2226b19a1392ddbf82aa65199b10aee035c80aee
falco2202 <giaont2001@gmail.com>
2023-09-08 02:04:55 +0000

    Fix ECR

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_ecr_repository" " ecr_repository" {
+resource "aws_ecr_repository" "ecr_repository" {
   name = local.ecr_repository_name
 }
 
