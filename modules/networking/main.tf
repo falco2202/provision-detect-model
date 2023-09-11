b2ab750b56c126730c93553614ce8dbcea594f4b
falco2202 <giaont2001@gmail.com>
2023-09-11 14:17:27 +0700

    update sg

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -41,16 +41,9 @@ resource "aws_security_group" "public_security_group" {
   ]
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
