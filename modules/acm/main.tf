0bac6b416833eb3f9e54e79f8176057bafb4838b
falco2202 <giaont2001@gmail.com>
2023-09-12 10:54:53 +0700

    fix

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -8,10 +8,10 @@ resource "aws_acm_certificate" "falcodev_cert" {
 }
 
 resource "aws_route53_record" "falcodev" {
-  name    = aws_acm_certificate.falcodev_cert.domain_validation_options[0].resource_record_name
+  name    = aws_acm_certificate.falcodev_cert.domain_validation_options.resource_record_name
   type    = "CNAME"
   zone_id = var.zone_id
 
-  records = [aws_acm_certificate.falcodev_cert.domain_validation_options[0].resource_record_value]
+  records = [aws_acm_certificate.falcodev_cert.domain_validation_options.resource_record_value]
   ttl     = 300
 }
